89e82e014ba4cfe2b6416ed2f2bbd936d83e9265
Bernardo Rebelo <bernasrebelo@github.com.pt>
2022-04-23 02:38:56 +0100

    spotify.py post artista/musica

--- client.py
+++ client.py
@@ -2,7 +2,7 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-r= requests.get(BASE + "utilizadores", {})
+r= requests.post(BASE + "artistas", {'artist_id' : '5K4W6rqBFWDnAN6FQUkS6x'})
 print (r.status_code)
 print (r.headers)
 print (r.text)
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from database import Database
 from flask import Flask, request
+from spotify import Spotify
 
 app = Flask(__name__)
 
 db = Database("spotify.db")
+spotify = Spotify("BQCBVN30CEO8-f0bPgzWXMXWC_rf-Gn4cOGPkTGnzoALlQg5xRgpwi3liBfXGtPn0ncjYNTMQRXWo8530BErGP1Snz_RLkFYy7ZFvcPxYy4hM8_Xj24dZL-ENLXD5CQKHJTu0FPqQ3XF9w")
 
 @app.route("/utilizadores", methods=["GET", "POST"])
 @app.route("/utilizadores/<int:id>", methods=["GET","PUT","DELETE"])
@@ -50,13 +52,51 @@ def utilizadores(id = None):
             else:
                 return "Utilizador não encontrado", 404
 
-@app.route("/artistas")
-def artistas():
-    return {"data": "Lista de artistas"}, 201
 
-@app.route("/musicas")
+@app.route("/artistas", methods=["POST"])
+def artistas():
+    
+    if request.method == "POST":
+        try:
+            artist_id = request.form["artist_id"]
+        except:
+            return "Erro: artist_id é obrigatório", 400
+        query = db.query("SELECT * FROM artistas WHERE id_spotify = ?", (artist_id,))
+        print(query)
+        if query:
+            return "Utilizador já existe", 409
+        else:
+            result = spotify.get_artist(artist_id)
+            if result:
+                db.query("INSERT INTO artistas (id_spotify, nome) VALUES (?, ?)", (artist_id, result))
+                return result, 201
+            else:
+                return "Artista não encontrado", 404
+
+@app.route("/musicas", methods=["POST"])
 def musicas():
-    return "Lista de musicas"
+    
+    if request.method == "POST":
+        try:
+            track_id = request.form["track_id"]
+        except:
+            return "Erro: track_id é obrigatório", 400
+        query = db.query("SELECT * FROM musicas WHERE id_spotify = ?", (track_id,))
+        if query:
+            return "Música já existe", 409
+        else:
+            result_track = spotify.get_track(track_id)
+            if result_track:
+                query = db.query("SELECT * FROM artistas WHERE id_spotify = ?", (result_track['artist_id'],))
+                if query:
+                    db.query("INSERT INTO musicas (id_spotify, nome, id_artista) VALUES (?, ?, ?)", (track_id, result_track['name'], result_track['artist_id']))
+                    return result_track['name'], 201
+                else:
+                    db.query("INSERT INTO artistas (id_spotify, nome) VALUES (?, ?)", (result_track['artist_id'], result_track['artist_name']))
+                    db.query("INSERT INTO musicas (id_spotify, nome, id_artista) VALUES (?, ?, ?)", (track_id, result_track['name'], result_track['artist_id']))
+                    return result_track['name'], 201
+            else:
+                return "Música não encontrada", 404
 
 if __name__ == "__main__":
     app.run(debug=True)
